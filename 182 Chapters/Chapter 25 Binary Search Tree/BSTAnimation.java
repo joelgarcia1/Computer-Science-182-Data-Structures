96a5b6feecd3519146d508b2d7a76b860502794f
Joel Garcia Valencia <joel.garciavalencia.372@my.csun.edu>
2019-05-09 22:18:16 -0700

    Creates an animation for Binary search tree

--- 182 Chapters/Chapter 25 Binary Search Tree/BSTAnimation.java
+++ 182 Chapters/Chapter 25 Binary Search Tree/BSTAnimation.java
@@ -1,60 +1,16 @@
-import javafx.application.Application; 
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField; 
-import javafx.scene.layout.BorderPane; 
-import javafx.scene.layout.HBox;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-
-public class BSTAnimation extends Application {
-   @Override // Override the start method in the Application class
- public void start(Stage primaryStage) {
- BST<Integer> tree = new BST<>(); // Create a tree
- BorderPane pane = new BorderPane();
- BTView view = new BTView(tree); // Create a BTView
-    pane.setCenter(view);
-
- TextField tfKey = new TextField();
- tfKey.setPrefColumnCount(3);
-      tfKey.setAlignment(Pos.BASELINE_RIGHT);
- Button btInsert = new Button("Insert");
- Button btDelete = new Button("Delete");
- HBox hBox = new HBox(5);
- hBox.getChildren().addAll(new Label("Enter a key: "),tfKey, btInsert, btDelete);
-hBox.setAlignment(Pos.CENTER);
-pane.setBottom(hBox);
-btInsert.setOnAction(e -> { int key = Integer.parseInt(tfKey.getText());
-if (tree.search(key)) { // key is in the tree already
-   view.displayTree();
-view.setStatus(key + " is already in the tree"); } 
-else {
-tree.insert(key); // Insert a new key view.displayTree();
-view.setStatus(key + " is inserted in the tree");
-
-
-ibtDelete.setOnAction(e -> {
-int key = Integer.parseInt(tfKey.getText());
-if (!tree.search(key)) { // key is not in the tree
-    view.displayTree();
-view.setStatus(key + " is not in the tree"); } else {
-tree.delete(key); // Delete a key view.displayTree();
-view.setStatus(key + " is deleted from the tree");
+public class BSTAnimation extends JFrame implements ActionListener
+{
+JScrollPane scrollpane;
+DisplayPanel panel;
+static Mytree t;
+static DisplaySimpleTree dt;
+JtextField textField;
 }
-});
-
-Scene scene = new Scene(pane, 450, 250);
-primaryStage.setTitle("BSTAnimation"); 
-primaryStage.setScene(scene); // Place the scene in the stage
-primaryStage.show(); // Display the stage
-}
-}
-
-public void displayTree() { 
-this.getChildren().clear(); // Clear the pane if (tree.getRoot() != null) {
-// Display tree recursively
-this.getChildren().clear(); // Clear the pane
-if((tree.getRoot() != null) {
- displayTree(tree.getRoot(),getWidth() / 2, vGap,);
