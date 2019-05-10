e2264f8366f6f7ba8a90f0240738852fd4d79662
Joel Garcia Valencia <joel.garciavalencia.372@my.csun.edu>
2019-05-09 23:40:12 -0700

    Update MyAbstractClass.java

--- 182 Chapters/Chapter 24 Implementin Lists/MyAbstractClass.java
+++ 182 Chapters/Chapter 24 Implementin Lists/MyAbstractClass.java
@@ -1,4 +1,45 @@
-public class MyAbstractClass
+public class MyAbstractClass<E> implements MyList<E>
 {
+protected int size = 0;
 
+//Create a default list
+protected MyAbstractList(){
+}
+
+// create a list from an array of objects
+protected MyAbstractList(E[] objects)     //contructor
+{
+for(int i = 0; i < objects.length; i++)
+add(size, e);
+}
+
+@Override   // add a new element at the end of the list
+public void add(E e)    implement add(E e)
+{
+add(size e);
+}
+
+@Override
+public boolean isEmpty()
+{
+return size == 0;
+}
+
+@Override
+public int size()
+{
+return size;
+}
+
+@Override
+public boolean remove(E e)
+{
+if(indexOf(e) > = 0)
+{
+remove(indexOf(e));
+return true;
+}
+else 
+   return false;
+}
 }
