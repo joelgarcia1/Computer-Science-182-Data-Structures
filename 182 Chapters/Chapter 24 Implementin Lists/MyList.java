a20dc43c3f9c81c0e6c7f069e094a0e66991d7b4
Joel Garcia Valencia <joel.garciavalencia.372@my.csun.edu>
2019-05-09 23:17:12 -0700

    MyList

--- 182 Chapters/Chapter 24 Implementin Lists/MyList.java
+++ 182 Chapters/Chapter 24 Implementin Lists/MyList.java
@@ -15,4 +15,36 @@ public boolean contains(E e);
 //return the element from this list at the specified index 
 public E get(int index);
 
+//returns the index of the first matching element in this list
+// returns -1 if no match
+public int indexOf(E e);
+
+//return true if this list doesn't contain any elements
+public boolean isEmpty();
+
+// return the index of the last matching element in this list
+//return -1 if no match
+public int lastIndexOf(E e);
+
+/* Remove the first occurrence of the element e from this list
+shift any subsequent elements to the left
+returns true if the element is removed */
+
+public boolean remove(E e);
+
+/* Remove the element at the specified postition 
+ and shifts any subsequent elements to the left
+return the element that was removed from the list
+
+*/
+public E remove(int index);
+
+/*replace the element at the specified position in this list
+with the specified element and return the old element
+*/
+
+public Object set(int index, E e);
+
+//returns the number of elements in this list
+public int size();
 }
